a74bb2a61f4c8eeb9a2e3edc51bfee2c1b09ab72
Allan Pugliese <allan_pugliesi@outlook.com>
2024-03-25 15:21:09 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/allan/cursomc/repositeories/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package com.allan.cursomc.repositeories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.allan.cursomc.domain.Categoria;
+
+
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+	
+
+}
