21dd56e813c59afd453b0956b77d3d2286659a61
Allan Pugliese <allan_pugliesi@outlook.com>
2024-03-26 13:09:57 -0300

    Criando operação de instanciação

--- src/main/java/com/allan/cursomc/Cursomc1Application.java
+++ src/main/java/com/allan/cursomc/Cursomc1Application.java
@@ -1,13 +1,35 @@
 package com.allan.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Cursomc1Application {
+import com.allan.cursomc.domain.Categoria;
+import com.allan.cursomc.repositeories.CategoriaRepository;
 
+@SpringBootApplication
+public class Cursomc1Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
