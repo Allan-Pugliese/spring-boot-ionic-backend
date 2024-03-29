bbcfc351b28bc815539d76027d68f626e9a67c4e
Allan Pugliese <allan_pugliesi@outlook.com>
2024-03-29 14:54:57 -0300

    Estado Pagamento, Pagamento e correção Properties

--- src/main/java/com/allan/cursomc/Cursomc1Application.java
+++ src/main/java/com/allan/cursomc/Cursomc1Application.java
@@ -1,5 +1,6 @@
 package com.allan.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.allan.cursomc.domain.Cidade;
 import com.allan.cursomc.domain.Cliente;
 import com.allan.cursomc.domain.Endereco;
 import com.allan.cursomc.domain.Estado;
+import com.allan.cursomc.domain.Pagamento;
+import com.allan.cursomc.domain.PagamentoComBoleto;
+import com.allan.cursomc.domain.PagamentoComCartao;
+import com.allan.cursomc.domain.Pedido;
 import com.allan.cursomc.domain.Produto;
+import com.allan.cursomc.domain.enums.EstadoPagamento;
 import com.allan.cursomc.domain.enums.TipoCliente;
 import com.allan.cursomc.repositeories.CategoriaRepository;
 import com.allan.cursomc.repositeories.CidadeRepository;
 import com.allan.cursomc.repositeories.ClienteRepository;
 import com.allan.cursomc.repositeories.EnderecoRepository;
 import com.allan.cursomc.repositeories.EstadoRepository;
+import com.allan.cursomc.repositeories.PagamentoRepository;
+import com.allan.cursomc.repositeories.PedidoRepository;
 import com.allan.cursomc.repositeories.ProdutoRepository;
 
 @SpringBootApplication
@@ -42,6 +50,16 @@ public class Cursomc1Application implements CommandLineRunner {
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	public Cursomc1Application() {
+		// TODO Auto-generated constructor stub
+	}
+
 	Cursomc1Application(ProdutoRepository produtoRepository) {
 		this.produtoRepository = produtoRepository;
 	}
@@ -52,6 +70,8 @@ public class Cursomc1Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		// Definindo formato de horas
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
 		// Instanciando categoria
 		Categoria cat1 = new Categoria(null, "Informatica");
@@ -103,11 +123,41 @@ public class Cursomc1Application implements CommandLineRunner {
 		Endereco e3 = new Endereco(null, "Rua Cardoso", "21", "Loja", "Centro", "29048944", cli3, c3);
 		Endereco e4 = new Endereco(null, "Rua Potigua", "77", "casa", "Amparo", "22578928", cli3, c3);
 
+		// Instanciando Pedidos
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/03/2024 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("12/04/2024 12:00"), cli2, e2);
+		Pedido ped3 = new Pedido(null, sdf.parse("29/03/2024 20:30"), cli3, e4);
+		Pedido ped4 = new Pedido(null, sdf.parse("29/03/2024 21:30"), cli3, e3);
+
 		cli1.getEnderecos().addAll(Arrays.asList(e1));
 
 		cli2.getEnderecos().addAll(Arrays.asList(e2));
 
 		cli3.getEnderecos().addAll(Arrays.asList(e3, e4));
+
+		// Instanciando pagamento
+
+		Pagamento pag1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, null);
+		ped1.setPagamento(pag1);
+
+		Pagamento pag2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("30/05/2024 14:40"), null);
+		ped2.setPagamento(pag2);
+
+		Pagamento pag3 = new PagamentoComCartao(null, EstadoPagamento.CANCELADO, ped3, 2);
+		ped3.setPagamento(pag3);
+
+		Pagamento pag4 = new PagamentoComBoleto(null, EstadoPagamento.QUITADO, ped4, sdf.parse("30/03/2024 00:00"),
+				sdf.parse("29/03/2024 23:25"));
+		ped4.setPagamento(pag4);
+
+		// Adicionando pedidos aos clientes
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1));
+		cli2.getPedidos().addAll(Arrays.asList(ped2));
+		cli3.getPedidos().addAll(Arrays.asList(ped3, ped4));
+
+		// Salvando os dados
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
@@ -115,6 +165,8 @@ public class Cursomc1Application implements CommandLineRunner {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3, e4));
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2, ped3, ped4));
+		pagamentoRepository.saveAll(Arrays.asList(pag1, pag2, pag3, pag4));
 	}
 
 }
---
+++ src/main/java/com/allan/cursomc/domain/Pedido.java
@@ -0,0 +1,115 @@
+package com.allan.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+public class Pedido implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date instante;
+
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
+	private Pagamento pagamento;
+
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+
+	@ManyToOne
+	@JoinColumn(name = "endereco_id")
+	private Endereco enderecoDeEntrega;
+
+	public Pedido() {
+
+	}
+
+	public Pedido(Integer id, Date instant, Cliente cliente, Endereco enderecoDeEntrega) {
+		super();
+		this.id = id;
+		this.instante = instant;
+		this.cliente = cliente;
+		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getInstant() {
+		return instante;
+	}
+
+	public void setInstant(Date instant) {
+		this.instante = instant;
+	}
+
+	public Pagamento getPagamento() {
+		return pagamento;
+	}
+
+	public void setPagamento(Pagamento pagamento) {
+		this.pagamento = pagamento;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Endereco getEnderecoDeEntrega() {
+		return enderecoDeEntrega;
+	}
+
+	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
+		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
